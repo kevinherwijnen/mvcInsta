b926dcad03f70bd388dc7763582254db527e7bbc
kevinherwijnen <35597130+kevinherwijnen@users.noreply.github.com>
2018-05-22 13:05:05 +0200

    updaet

--- Views/partials/head.php
+++
@@ -1,9 +0,0 @@
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-
-<!-- alles wat in de head zit -->
-<link rel="stylesheet" href="style/bootstrap/css/bootstrap.css">
-<link rel="stylesheet" href="style/bootstrap/css/bootstrap-theme.css">
-<link rel="stylesheet" href="style/style.css">
-<link rel="stylesheet" href="style/mediatags.css">
-
